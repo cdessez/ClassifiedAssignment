8ed5dd36d7c8b93aea8a92d6af4d8b1bda84b8d1
Cedric Dessez <cedric.dessez13@imperial.ac.uk>
2013-11-21 15:21:15 +0000

    Add arguments to SeqLaplaceInv

--- implementation/SeqLaplaceInv.cpp
+++ implementation/SeqLaplaceInv.cpp
@@ -1,13 +1,40 @@
 #include "SeqLaplaceInv.h"
 
-SeqLaplaceInv::SeqLaplaceInv(): LaplaceInv() {
+static int defaultN = 50;
+static int defaultM = 15;
+
+SeqLaplaceInv::SeqLaplaceInv(): LaplaceInv(), N(defaultN), 
+        M(defaultM), Cm(defaultM) {
+  for (int i = 0; i < M; i++)
+    Cm[i] = C(i,M);
 }
 
-SeqLaplaceInv::SeqLaplaceInv(complex<double> (*func)(double,double)): LaplaceInv(func) {
+SeqLaplaceInv::SeqLaplaceInv(complex<double> (*func)(double,double)): 
+        LaplaceInv(func), N(defaultN), M(defaultM), Cm(defaultM) {
+  for (int i = 0; i < M; i++)
+    Cm[i] = C(i,M);
+}
+
+SeqLaplaceInv::SeqLaplaceInv(complex<double> (*func)(double,double), int N, 
+                              int M): 
+        LaplaceInv(func), N(N), M(M), Cm(M) {
+  for (int i = 0; i < M; i++)
+    Cm[i] = C(i,M);
 }
 
 double SeqLaplaceInv::operator()(double t){
   //TODO
   return t;
 }
+
+int SeqLaplaceInv::C(int k, int m){
+  long res = 1; 
+  const long mk = m - k;
+  for (int i = m; i > k; i--)
+    res *= i;
+  for (int i = 1; i <= mk; i++)
+    res /= i;
+  return static_cast<int>(res);
+}
+
 
--- implementation/SeqLaplaceInv.h
+++ implementation/SeqLaplaceInv.h
@@ -8,10 +8,15 @@ class SeqLaplaceInv : public LaplaceInv {
   public:
     SeqLaplaceInv();
     SeqLaplaceInv(complex<double> (*func)(double,double));
+    SeqLaplaceInv(complex<double> (*func)(double,double), int N, int M);
     double operator()(double t);
   protected:
+    int N;
+    vector<int> Cm;
+    int M;
 
   private:
+    inline int C(int k, int m);
 };
 
 #endif
