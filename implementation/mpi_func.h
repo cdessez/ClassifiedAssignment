69953d3b7791e823dc7b838c5186a32b4f150f63
vg213 <vg213@ic.ac.uk>
2013-11-28 15:51:21 +0000

    removed mpi1_openmp

--- implementation/mpi_func.h
+++ implementation/mpi_func.h
@@ -14,9 +14,6 @@ void mpi1(vector<double> &input, vector<double> &output, int N, int M, int input
 void mpi2(vector<double> &input, vector<double> &output, int N, int M, int input_size,
  int my_rank, int pool_size, bool iammaster, int MASTER_RANK);
 
-void mpi1_openmp(vector<double> &input, vector<double> &output, int N, int M, int input_size,
- int my_rank, int pool_size, bool iammaster, int MASTER_RANK);
-
 void mpi2_openmp(vector<double> &input, vector<double> &output, int N, int M, int input_size,
  int my_rank, int pool_size, bool iammaster, int MASTER_RANK);
 
